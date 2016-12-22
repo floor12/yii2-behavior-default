2d137cfad592470c942eebb5d31afa460bf78917
floor12 <primelifecoder@gmail.com>
2016-12-22 13:38:38 +0100

    init

---
+++ src/DefaultBehavior.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: floor12
+ * Date: 21.12.2016
+ * Time: 19:36
+ */
+
+namespace floor12\default-behavior;
+
+use yii\base\Behavior;
+use yii\db\ActiveRecord;
+
+class DefaultBehavior extends Behavior
+{
+    public $create_user_id = 'create_user_id';
+    public $update_user_id = 'update_user_id';
+    public $created = 'created';
+    public $updated = 'updated';
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'saveCreator',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'saveUpdator'
+        ];
+    }
+
+    public function saveCreator()
+    {
+        $this->owner->$this->create_user_id = \Yii::$app->user->id;
+        $this->owner->$this->created = time();
+        $this->owner->$this->update_user_id = \Yii::$app->user->id;
+        $this->owner->$this->updated = time();
+    }
+
+    public function saveUpdator()
+    {
+        $this->owner->$this->update_user_id = \Yii::$app->user->id;
+        $this->owner->$this->updated = time();
+    }
+
+
+}
